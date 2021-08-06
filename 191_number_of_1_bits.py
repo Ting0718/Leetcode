e1599d13f6a4c0a62cedc16738d4e60fcc5de9d5
Shireen <shireen0718@gmail.com>
2021-08-06 23:48:29 +0800

    add another solution to 191

--- 191_number_of_1_bits.py
+++ 191_number_of_1_bits.py
@@ -1,3 +1,13 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         return bin(n).count("1")
+
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        while n != 0:
+            res += (n % 2) & 1
+            n = n >> 1
+
+        return res
