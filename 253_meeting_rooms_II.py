ba5694ec8597eebc73d55982b3f4f67293949d5d
Shireen <shireen0718@gmail.com>
2021-10-11 16:23:10 -0700

    add 253

---
+++ 253_meeting_rooms_II.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        start = sorted([i[0] for i in intervals])
+        end = sorted([i[1] for i in intervals])
+
+        count = res = 0
+        e = s = 0
+
+        while s < len(intervals):
+            if start[s] < end[e]:
+                count += 1
+                s += 1
+            else:
+                count -= 1
+                e += 1
+            
+            res = max(count, res)
+        
+        return res
