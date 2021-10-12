f8337c4aa22b3f235c27100e81d9d1031a7e696e
Shireen <shireen0718@gmail.com>
2021-10-12 15:41:59 -0700

    add tree directory

--- Array/027_remove_elements.py
+++ Array/027_remove_elements.py
@@ -8,3 +8,14 @@ class Solution:
                 index += 1
 
         return len(nums) # index
+
+'''Better Solution'''
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        length = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[length] = nums[i]
+                length += 1
+
+        return length
