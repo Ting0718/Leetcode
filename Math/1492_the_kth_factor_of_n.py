4171b1d12d9e28185193d970a76c352857662b2e
Shireen <shireen0718@gmail.com>
2021-10-13 14:00:01 -0700

    add 1492

---
+++ Math/1492_the_kth_factor_of_n.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        res = []
+        for i in range(1, n + 1):
+            if n % i == 0:
+                res.append(i)
+            if k == len(res):
+                break
+
+        if k - 1 < len(res):
+            return res[k - 1]
+
+        return -1
