5d77407da6aaed68e4c99abd212f96388f12e1e6
Shireen <shireen0718@gmail.com>
2021-11-03 21:14:46 -0700

    Add more problems

--- Tree/226_invert_binary_tree.py
+++ Tree/226_invert_binary_tree.py
@@ -11,6 +11,25 @@ class Solution:
 
         return root
 
+'''bfs solution'''
+
+
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return root
+
+        queue = [root]
+        while queue:
+            for i in range(len(queue)):
+                current = queue.pop(0)
+                current.left, current.right = current.right, current.left
+                if current.left:
+                    queue.append(current.left)
+                if current.right:
+                    queue.append(current.right)
+
+        return root
 
 class Solution:
     def invertTree(self, root: TreeNode) -> TreeNode:
