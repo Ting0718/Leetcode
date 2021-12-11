2241cca9212a7382d7c39dc3a272a5b488d94afe
Shireen <shireen0718@gmail.com>
2021-12-10 17:40:22 -0800

    restructure the directories

--- String/1143_longest_common_subsequence.py
+++ String/1143_longest_common_subsequence.py
@@ -1,3 +1,21 @@
+'''recursion'''
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        def lcs(i, j, length):
+            if i == len(text1) or j == len(text2):
+                return length
+            
+            if text1[i] == text2[j]:
+                return lcs(i + 1, j + 1, length + 1)
+            
+            else:
+                return max(lcs(i + 1, j, length), lcs(i, j + 1, length))
+        
+        
+        return lcs(0, 0, 0)
+
+
+'''dynamic programming'''
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         dp = [[0 for j in range(len(text2) + 1)] for i in range(len(text1) + 1)]  # +1 for a column of 0
